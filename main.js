d234f9880fe38fcebef20b41462a7b9a02202e53
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2021-12-22 20:27:14 +0000

    working tests

--- main.js
+++ main.js
@@ -3,29 +3,11 @@
 */
 // The equation to solve BMI is Mass / (Height * Height);
 // Tom weighs 78kg and is 1.80m tall, Betty weights 60kg and is 1.50m tall.
-// Please calculate and return who has the biggest BMI rounded to the nearest Interger. 
+// Please calculate and return who has the biggest BMI rounded down to the nearest Integer. 
 
-let tomMass = 78;
-let tomHeight = 1.80;
-let bettyMass = 60;
-let bettyHeight = 1.50;
-
-let tomBMI = Math.floor(tomMass / (tomHeight * tomHeight));
-let bettyBMI = Math.floor(bettyMass / (bettyHeight * bettyHeight));
-
-if(tomBMI > bettyBMI){
-  console.log(`Tom's BMI is bigger than Betty's`);
-  return `Tom's BMI ${tomBMI} is bigger than Betty's ${bettyBMI}`;
-}
-else {
-  console.log(`Betty's BMI is bigger than Tom's`);
-  return `Betty's BMI ${bettyBMI} is bigger than Tom's ${tomBMI}`;
-}
 //👉 Write the function your CodeWarriors will start with below here:
 function calculateBMI(mass, height) {
   return Math.floor(mass / (height * height));
 }
-console.log(calculateBMI(78, 1.80))
 
-
-
+export default calculateBMI
--- main.test.js
+++ main.test.js
@@ -1,14 +1,27 @@
 //👉 Write your tests below here:
-
+import calculateBMI from './main.js'
 
 test("check tomBMI", function(){
-    const tomBMI = 24;
-    const expected = 24;
-    expect(tomBMI).toBe(expected)
+    expect(calculateBMI(78, 1.80)).toBe(24)
 })
 
 test("check bettyBMI", function(){
-    const bettyBMI = 26;
+    const bettyMass = 60;
+    const bettyHeight = 1.50;
     const expected = 26;
-    expect(bettyBMI).toBe(expected)
+    expect(calculateBMI(bettyMass, bettyHeight)).toBe(expected)
 })
+
+
+
+
+
+
+// if(tomBMI > bettyBMI){
+//   console.log(`Tom's BMI is bigger than Betty's`);
+//   return `Tom's BMI ${tomBMI} is bigger than Betty's ${bettyBMI}`;
+// }
+// else {
+//   console.log(`Betty's BMI is bigger than Tom's`);
+//   return `Betty's BMI ${bettyBMI} is bigger than Tom's ${tomBMI}`;
+// }
