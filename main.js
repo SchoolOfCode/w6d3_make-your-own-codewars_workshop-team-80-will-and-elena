4225bc645c9fcd36258c3a307d28647f187afd01
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2021-12-22 21:12:11 +0000

    Harder challenge

--- main.js
+++ main.js
@@ -3,11 +3,24 @@
 */
 // The equation to solve BMI is Mass / (Height * Height);
 // Tom weighs 78kg and is 1.80m tall, Betty weights 60kg and is 1.50m tall.
-// Please calculate and return who has the biggest BMI rounded down to the nearest Integer. 
+// Please calculate and return who has the biggest BMI rounded down to the nearest Integer.
 
 //👉 Write the function your CodeWarriors will start with below here:
-function calculateBMI(mass, height) {
+export function calculateBMI(mass, height) {
   return Math.floor(mass / (height * height));
 }
 
-export default calculateBMI
+export function pickBiggestBMI(people){
+  let maxName = undefined
+  let maxBMI = 0
+  for (let i = 0; i < people.length; i++) {
+    let p = people[i]
+    let bmi = calculateBMI(p.weight, p.height)
+    if (bmi > maxBMI) {
+      maxBMI = bmi
+      maxName = p.name
+    }
+  }
+
+  return maxName;
+}
--- main.test.js
+++ main.test.js
@@ -1,5 +1,5 @@
 //👉 Write your tests below here:
-import calculateBMI from './main.js'
+import {calculateBMI, pickBiggestBMI} from './main.js'
 
 test("check tomBMI", function(){
     expect(calculateBMI(78, 1.80)).toBe(24)
@@ -12,16 +12,52 @@ test("check bettyBMI", function(){
     expect(calculateBMI(bettyMass, bettyHeight)).toBe(expected)
 })
 
+test("check bettyBMI", function(){
+    const bettyMass = 60;
+    const bettyHeight = 1.50;
+    const expected = 26;
+    expect(calculateBMI(bettyMass, bettyHeight)).toBe(expected)
+})
 
+test("pick the biggest", function(){
+    const people = [
+        {
+            name: "Tom",
+            weight: 78,
+            height: 1.80
+        },
+        {
+            name: "Betty",
+            weight: 60,
+            height: 1.50
+        }
+    ]
 
+    expect(pickBiggestBMI(people)).toBe('Betty')
+})
 
+test("pick the first if the same BMI", function(){
+    const people = [
+        {
+            name: "Tony",
+            weight: 70,
+            height: 1.78
+        },
+        {
+            name: "Steve",
+            weight: 78,
+            height: 1.78
+        },
+        {
+            name: "Eric",
+            weight: 78,
+            height: 1.78
+        }
+    ]
 
+    expect(pickBiggestBMI(people)).toBe('Steve')
+})
 
-// if(tomBMI > bettyBMI){
-//   console.log(`Tom's BMI is bigger than Betty's`);
-//   return `Tom's BMI ${tomBMI} is bigger than Betty's ${bettyBMI}`;
-// }
-// else {
-//   console.log(`Betty's BMI is bigger than Tom's`);
-//   return `Betty's BMI ${bettyBMI} is bigger than Tom's ${tomBMI}`;
-// }
+test("undefined if no people", function() {
+    expect(pickBiggestBMI([])).toBe(undefined)
+})
